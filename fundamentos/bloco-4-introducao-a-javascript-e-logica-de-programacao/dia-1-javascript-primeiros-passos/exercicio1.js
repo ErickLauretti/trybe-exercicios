b8d5444edf49069898ff8e405c41a4267a93d64f
Erick Lauretti <erick.nighyt@hotmail.com>
2022-08-18 19:20:58 -0300

    exercicio 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -1,9 +1,9 @@
 const a = 2;
-const b = 5;
-const c = 9;
+const b = 2;
+const c = 2;
 const condicao1 = true;
 const condicao2 = false;
-if (a || b || c % 2 == 0) {
+if (a || b || c % 3 == 0) {
     console.log(condicao1);
 } else {
     console.log(condicao2);
