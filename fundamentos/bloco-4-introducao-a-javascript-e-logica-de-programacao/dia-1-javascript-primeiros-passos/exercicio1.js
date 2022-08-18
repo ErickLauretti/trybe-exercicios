5b00f498fe70bb5f8bc820c5cb87671ddea9a4c9
Erick Lauretti <erick.nighyt@hotmail.com>
2022-08-18 18:19:54 -0300

    exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -1,8 +1,11 @@
 const a = 5;
 const b = 3;
+const c = 10;
 
-if (a > b) {
+if (a > b && a > c) {
     console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
 } else {
-    console.log(invalido);
+    console.log(c);
 }
