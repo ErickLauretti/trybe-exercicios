aacb9bc9410d26913c9db97c7418f12f2f6eb66a
Erick Lauretti <erick.nighyt@hotmail.com>
2022-08-18 19:19:47 -0300

    exercicio 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -1,21 +1,10 @@
-let porcentagem = -5;
-
-if (porcentagem >= 90) {
-    console.log('A');
-} else if (porcentagem >= 80) {
-    console.log('B');
-} else if (porcentagem >= 70) {
-    console.log('C');
-} else if (porcentagem >= 60) {
-    console.log('D');
-} else if (porcentagem >= 50) {
-    console.log('E');
-} else if (porcentagem < 50) {
-    console.log('F');
-} else if (porcentagem < 0) {
-    console.log('erro');
-} else if (porcentagem > 100) {
-    console.log('erro');
+const a = 2;
+const b = 5;
+const c = 9;
+const condicao1 = true;
+const condicao2 = false;
+if (a || b || c % 2 == 0) {
+    console.log(condicao1);
 } else {
-    console.log('erro');
+    console.log(condicao2);
 }
